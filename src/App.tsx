127ef7406aaaaae40a3cb2b9fa2a03a383602daa
J <98541904+DownMan01@users.noreply.github.com>
2025-09-13 21:43:33 +0800

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -16,6 +16,7 @@ import Privacy from "./pages/Privacy";
 import Terms from "./pages/Terms";
 import NotFound from "./pages/NotFound";
 import Verify from "./pages/Verify";
+import Success from "./pages/Success";
 
 const queryClient = new QueryClient();
 
@@ -53,6 +54,7 @@ const App = () => (
             <Route path="/privacy" element={<Privacy />} />
             <Route path="/terms" element={<Terms />} />
             <Route path="/verify" element={<Verify />} />
+            <Route path="/success" element={<Success />} />
             {/* Catch-all route */}
             <Route path="*" element={<NotFound />} />
           </Routes>
